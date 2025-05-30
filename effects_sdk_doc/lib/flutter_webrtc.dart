136c225715147d65f8fcc9cb45857006686589fa
uei <uei@tomsksoft.com>
2025-05-30 19:03:37 +0300

    Add Effects SDK docs

---
+++ effects_sdk_doc/lib/flutter_webrtc.dart
@@ -0,0 +1,6 @@
+/// Flutter WebRTC with Effects SDK integration.
+library flutter_webrtc;
+
+export 'src/effects_sdk_constants.dart';
+export 'src/effects_sdk_image.dart' show EffectsSdkImage, RawImageFormat;
+export 'src/effects_sdk.dart';
